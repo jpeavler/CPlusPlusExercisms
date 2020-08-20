c47194f92719323d71152203b91f0c22ddd5f260
Joshua Peavler <joshuaapeavler@gmail.com>
2020-08-20 14:56:03 -0600

    Fixed issue where isograms with multilple hyphens or spaces were rejected, now isogram exercism is complete

--- isogram/isogram_test.cpp
+++ isogram/isogram_test.cpp
@@ -14,7 +14,7 @@ TEST_CASE("empty_string")
     REQUIRE(expected == actual);
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
+
 TEST_CASE("isogram_with_only_lower_case_characters")
 {
     const bool actual = isogram::is_isogram("isogram");
@@ -122,5 +122,5 @@ TEST_CASE("same_first_and_last_characters")
 
     REQUIRE(expected == actual);
 }
-
+#if defined(EXERCISM_RUN_ALL_TESTS)
 #endif
