f4755547fa0b7c7b772e383d8f44771928dda1b0
Joshua Peavler <joshuaapeavler@gmail.com>
2020-08-20 09:56:52 -0600

    Downloaded and completed isogram exercism in C++

---
+++ isogram/isogram.cpp
@@ -0,0 +1,16 @@
+#include "isogram.h"
+
+namespace isogram {
+    bool is_isogram(std::string str) {
+        int letterCount [26] = {0}; //Initializes a tally for each letter
+        for(int i = 0; i < (int) str.length(); i++) {
+            short letterIndex = tolower(str[i]) - 'a';  //char a is subtracted to match letterCount index
+            if(letterCount[letterIndex] > 0) {
+                return false;
+            } else {
+                letterCount[letterIndex]++;
+            }
+        }
+        return true;
+    }
+}  // namespace isogram
